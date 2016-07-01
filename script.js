0ec94710f5843e9a5e6e75668da0549f1a85d078
Stephanie <stephkwok94@gmail.com>
2016-07-01 10:39:33 +0200

    api works

--- script.js
+++ script.js
@@ -17,9 +17,18 @@ app.controller("homeCtrl", function($scope,$location,$http,$window){
 
 });
 
-
 //portal page controller
 app.controller("portalCtrl", function($scope,$location,$http,$window){
-	
+	$http({
+		method: "GET",
+		url: 'https://private-anon-c529c0f64-parcelninja.apiary-mock.com/api/v1/tracking/WIA1340707',
+		headers :{
+		'Content-Type': 'application/json',
+		'Accept': 'application/json',
+		'Authorization': '6aed16cf-f549-43d1-9f16-07056ea8b1a3',	
+		}
+	}).then(function(response){
+		console.log(response);
+	})
 
 });
